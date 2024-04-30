0bb4c20729d1a533f80e5a24b00f2ac1ff806f4a
Evgeniya D. <Evgeniya7000@rambler.ru>
2024-04-30 15:24:17 +1200

    Added code tests

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -1,19 +1,76 @@
-plugins {
-    id 'java'
-}
 
-group = 'ru.netology'
-version = '1.0-SNAPSHOT'
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-repositories {
-    mavenCentral()
-}
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-dependencies {
-    testImplementation 'org.junit.jupiter:-junit-jupiter:5.7.0'
-    testImplementation 'org.seleniumhq.selenium:selenium-java:3.141.59'
-}
 
-test {
-    useJUnitPlatform()
+public class CallbackTest {
+    private WebDriver driver;
+
+
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+        //System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    public void afterEach() {
+        driver.quit();
+        driver = null;
+
+    }
+
+    @Test
+    public void shouldBeSuccessTest() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Евгений Владиславович Пупкин");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79098312222");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        var actualElement = driver.findElement(By.cssSelector("[data-test-id=order-success]"));
+        var actualText = actualElement.getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
+        assertTrue(actualElement.isDisplayed());
+    }
+    @Test
+    public void IfIncorrectNameTest() {
+        driver.findElement(By.xpath("//span[@data-test-id='name']//input")).sendKeys("Evgeniy");
+        driver.findElement(By.xpath("//span[@data-test-id='phone']//input")).sendKeys("+79098312222");
+        driver.findElement(By.xpath("//label[@data-test-id='agreement']")).click();
+        driver.findElement(By.xpath("//button[contains(@class,'button')]")).click();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.",
+                driver.findElement(By.xpath("//span[@data-test-id='name'][contains(@class, 'input_invalid')]//span[@class='input__sub']")).getText().trim());
+    }
+    @Test
+    public void IfEmptyNameTest() {
+
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79098312222");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        assertEquals("Поле обязательно для заполнения",
+                driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText().trim());
+        assertTrue(driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).isDisplayed());
+    }
+
+
 }
